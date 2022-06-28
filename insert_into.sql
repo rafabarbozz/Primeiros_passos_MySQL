f028a93bdc26660359ba367c6c4af68b5adcccc1
Rafael Barboza <rafael.almeida1096@gmail.com>
2022-06-28 19:26:27 -0300

    Como adicionar dados a uma tabela

---
+++ insert_into.sql
@@ -0,0 +1,9 @@
+use cadastro;
+
+insert into pessoas values
+(default, 'Claudio', '1975-4-22', 'M', '99.0', '2.15', 'Brasil'),
+(default, 'Pedro', '1999-12-03', 'M', '87', '2', default),
+(default, 'Janaina', '1987-11-12', 'F', '75.4', '1.66', 'EUA');
+
+select * from pessoas;
+ 
