a1dc15f87c42d720c83b7cb5dd68389b02c69a1f
Rafael Barboza <rafael.almeida1096@gmail.com>
2022-06-28 22:06:49 -0300

    Organizando

--- altertable2.sql
+++ altertable2.sql
@@ -1,9 +1,9 @@
 create table if not exists cursos(
-nome varchar(30) not null unique,
-descricao text,
-carga int unsigned,
-totaulas int unsigned,
-ano year default '2016'
+	nome varchar(30) not null unique,
+	descricao text,
+	carga int unsigned,
+	totaulas int unsigned,
+	ano year default '2016'
 ) default charset=utf8;
 
 alter table cursos
