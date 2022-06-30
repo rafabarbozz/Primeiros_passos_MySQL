24ec8158b976f374ff593d24ff48ad45462e0828
Rafael Barboza <rafael.almeida1096@gmail.com>
2022-06-30 18:11:59 -0300

    Funções para procurar palavras que parecem com a sugerida

---
+++ Select/like.sql
@@ -0,0 +1,14 @@
+select * from cursos
+where nome like '%p';
+
+select * from cursos
+where nome like '%p';
+
+select * from cursos
+where nome like '%p%';
+
+select * from cursos
+where nome like 'ph%p';
+
+select * from gafanhotos
+where nome like '%_silva%';
